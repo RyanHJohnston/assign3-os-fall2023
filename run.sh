7872a3ea4cd0470b1346a574ba5fda2216939010
ryanhjohnston <ryanhjohnstoncollege@gmail.com>
2023-10-31 05:30:24 -0500

    finalized project, submitting this

--- run.sh
+++ run.sh
@@ -4,9 +4,10 @@ make all
 
 for i in {1..4};
 do
-    make fifo >> output.txt
-    make sjf >> output.txt
-    make pr >> output.txt
+    make fifo
+    make sjf
+    make pr
 done
 
-make clean
+printf "\nThank you for using this program!\n"
+printf "Email me at ryan.johnston@utsa.edu for any questions or concerns.\n"
